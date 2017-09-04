74e60fd8c65dcf3dfc2f344e299340b6c7f29086
Thomas Buckley-Houston <tom@tombh.co.uk>
2017-09-04 19:46:02 +0700

    Removed files ready for gh-pages site

--- laoup.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-
-require 'json'
-require 'encounters/lib/aggregate'
-
-aggregate = Aggregate.new
-#Aggregate.download
-aggregate.compile_all
-aggregate.save_to_disk
